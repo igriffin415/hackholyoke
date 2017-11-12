17371624a1b878cfb2df5d5692aff3003bc6ce89
claraCoder <grottoemma@gmail.com>
2017-11-12 09:51:45 -0500

    changes

---
+++ MyApplication/app/src/main/java/com/ie/hackerthongame/hardwareHero.java
@@ -0,0 +1,13 @@
+package com.ie.hackerthongame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hardwareHero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hardware_hero);
+    }
+}
