25c4341e419a5450f1514607a440c893c30c4679
claraCoder <grottoemma@gmail.com>
2017-11-12 08:35:39 -0500

    restart

--- MyApplication/app/src/main/java/com/ie/hackerthongame/End.java
+++ MyApplication/app/src/main/java/com/ie/hackerthongame/End.java
@@ -1,8 +1,15 @@
 package com.ie.hackerthongame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.ie.hackerthongame.foogle.Foogle;
+import com.ie.hackerthongame.helperFiles.Story;
 
 public class End extends Activity {
 
@@ -14,4 +21,8 @@ public class End extends Activity {
 
     @Override
     public void onBackPressed() {/* we only move forward. */ }
+
+    public void backToMain(View view){
+        startActivity( new Intent(this, MainScreen.class));
+    }
 }
